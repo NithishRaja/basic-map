23f19c98683d76f9fca560eee5c19bc2f9ccd462
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2017-08-19 23:59:27 +0530

    added TODO list

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var app = require('./server/server.js');
 
+//TODO write tests for the routes
+
+//TODO provide proper documentation
+
 app.listen(app.get('port'), function(){
   console.log('server is listening at port %d, press CTRL+C to close server', app.get('port'));
