895c386775dd3ba24b8dfc7e138e56f2406a7095
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-27 13:13:26 +0530

    rework credentials.js

--- credentials.js
+++ credentials.js
@@ -1,8 +1,33 @@
 
-module.exports = {
-  google: {
-    googlemaps: {
-      apikey: "AIzaSyCJkSm8qeTg2sBpXjIRAPQE0GMBFEK-C6I"
+// Container for environments
+const environment = {};
+
+// Development environment parameters
+environment.development = {
+  'port' : 80,
+  'envName': "development",
+  'google': {
+    'googlemaps': {
+      'apikey': "AIzaSyCJkSm8qeTg2sBpXjIRAPQE0GMBFEK-C6I"
     }
   }
-}
+};
+
+// Production environment parameters
+environment.production = {
+  'port': 443,
+  'envName': "production",
+  'google': {
+    'googlemaps': {
+      'apikey': "AIzaSyCJkSm8qeTg2sBpXjIRAPQE0GMBFEK-C6I"
+    }
+  }
+};
+
+// Getting requested environment
+const requestedEnvironment = typeof(process.env.NODE_ENV)=="string"?process.env.NODE_ENV:"";
+
+// Setting selected environment
+const selectedEnvironment = typeof(environment[requestedEnvironment])=="object"?environment[requestedEnvironment]:environment.development;
+
+module.exports = selectedEnvironment;
